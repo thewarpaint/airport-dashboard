02558e8bb1a21ae9e519ece872e8ccccb49ecc5f
Eduardo Garcia <egarcia@ooyala.com>
2015-09-25 00:30:22 -0500

    Add autoprefixer to Gulp, upgrade SASS, remove engines from package.json

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp'),
     notify = require('gulp-notify'),
     sass = require('gulp-sass'),
     minifyCSS = require('gulp-minify-css'),
+    autoprefixer = require('gulp-autoprefixer'),
     paths = {
       js: ['app/main.js'],
       jsMin: 'app.min.js',
@@ -24,6 +25,10 @@ gulp.task('styles', function () {
   return gulp.src(paths.css)
     .pipe(sass())
     .pipe(concat(paths.cssMin))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(minifyCSS())
     .pipe(gulp.dest(paths.dist))
     .pipe(notify({ message: 'Finished compiling SASS and minifying CSS'}));
